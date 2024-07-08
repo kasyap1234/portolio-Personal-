845a08316c7c95f8730a8d8ab0bae27307a139f3
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2024-07-08 12:56:43 +0530

    added basic one

---
+++ src/lib/components/Skills.svelte
@@ -0,0 +1,11 @@
+<script lang="ts"> 
+export let skill: string; 
+</script> 
+
+<!-- <span class="bg-gray-500 "> 
+   
+</span>  -->
+<div class="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#333333] pl-4 pr-4">
+                <p class="text-[#FFFFFF] text-sm font-medium leading-normal">{skill}</p>
+              </div>
+              
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,49 @@
-<script>
+<script lang="ts">
 	import Navbar from "$lib/components/Navbar.svelte";
+	import Skills from "$lib/components/Skills.svelte";
 
- 
+
 </script> 
 <div class=" mt-6 "> 
 <Navbar/> 
 </div>
-<div class="flex flex-grow "> 
-    <div class="container  mt-20  ml-40 flex flex-row  "> 
+<div class="container flex-column flex-wrap">
+<div class=" ml-40 container "> 
+    <div class="container  mt-20  flex flex-column   "> 
         <div> 
         <h1 class="text-2xl mt-4 ml-10 font-medium text-gray-100"> Hi I'm    </h1>
         <h1 class="text-2xl mt-4 ml-10 font-medium text-gray-100"> Kasyap Dharanikota </h1>
         <p class="text-gray-400 ml-10 mt-4"> I am a full stack software developer with a passion for building software that is both functional and scalable. </p>
+        
 
         </div>
+                
+        
+
         </div>
+  
+ <div class="flex gap-3 ml-10 mt-10 flex-wrap">
+
+    <Skills skill='golang' /> 
+    <Skills skill='nodejs' /> 
+    <Skills skill='react' />
+    <Skills skill='svelte' />
+    <Skills skill='python' />
+    <Skills skill='AWS' />
+    <Skills skill='firebase' />
+    <Skills skill='mongodb' /> 
+    <Skills skill='postgresql' />
+    <Skills skill='Docker' /> 
+    <Skills skill='kubernetes' />
+    <Skills skill='git' />
+    <Skills skill='github' />
+    <Skills skill='Linux' /> 
+    <Skills skill='TailwindCSS' /> 
+    <Skills skill='nestjs'/> 
+
     </div>
-
+    </div>
+  </div>
  <style> 
  
  </style> 
